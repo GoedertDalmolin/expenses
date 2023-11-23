e8c599457bbb49076b9febcf00920136abbb3748
GoedertDalmolin <gbrunodalmolin@gmail.com>
2023-11-22 21:51:38 -0300

    Updated transaction widget styles.

--- lib/main.dart
+++ lib/main.dart
@@ -52,24 +52,36 @@ class MyHomePage extends StatelessWidget {
                   children: [
                     Container(
                       decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2
-                        )
+                          border: Border.all(
+                              color: Colors.purple,
+                              width: 2
+                          )
                       ),
                       margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
                       padding: EdgeInsets.all(10),
                       child: Text(tr.value.toString(),
-                      style: TextStyle(
-                        fontWeight:FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple
-                      ),),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple
+                        ),),
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(tr.title),
-                        Text(tr.date.toString()),
+                        Text(tr.title,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),),
+                        Text(tr.date.toString(),
+                          style: TextStyle(
+                            color: Colors.black38,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
                       ],
                     )
                   ],
